fdcee0e1291a7459c4095fcbc3118a1afb9880b8
Mike Steder <steder@gmail.com>
2014-07-29 23:12:21 -0500

    slightly nicer cat

--- cat.hs
+++ cat.hs
@@ -3,22 +3,12 @@ import System.IO
 import qualified Data.Map as Map
 
 catFile path = withFile path ReadMode (\handle -> do
-                                          print path
+                                          putStrLn $ "::" ++ path
                                           contents <- hGetContents handle
                                           putStr contents
                                           putStr "\n")
 
-printArg arg = do
-  print arg
-
-
-factorial :: Int -> Int
-factorial 0 = 1
-factorial n = n * factorial n - 1
-
-
 main = do
-  -- map catFile getArgs
   args <- getArgs
-  -- mapM printArg args
   mapM catFile args
+  putStrLn ""
